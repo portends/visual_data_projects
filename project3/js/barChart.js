26e2adf2436582747da491e2a7d89351fa863b60
swansobp <swansobp@mail.uc.edu>
2022-04-21 09:33:25 -0400

    Added Episode Count Bar Chart
    copied the code for bar charts from project 2 and used that, still needs to be styled according to our project 3 theme. Also did a lot of data preprocessing since d3 needs arrays and our data was in dictionary format

--- project3/js/barChart.js
+++ project3/js/barChart.js
@@ -126,7 +126,7 @@ class BarChart {
                 .style('top', (event.pageY + 15) + 'px')
                 .html(`
                 <div class="tooltip-title">${vis.config.x[index]}</div>
-                <div><i>Count: ${vis.config.y[index]} Specimans</div>
+                <div><i>Count: ${vis.config.y[index]} Episodes</div>
                 `);
             })
             .on('mouseleave', () => {
